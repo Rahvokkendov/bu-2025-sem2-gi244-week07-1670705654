b8975158f860f4a0101a0517bcd1e6856e4d7af5
Rahvokkendov <rahvokkendov@gmail.com>
2026-02-27 16:46:28 +0700

    Week07

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -1,18 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 10f;
-
+    GameObject go;
+    PlayerController player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        go = GameObject.FindGameObjectWithTag("Player");
+        player = go.GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime);
+        //GameObject go = GameObject.Find("Player");
+        //PlayerController player = go.GetComponent<PlayerController>();
+        if(player.isGameOver == false)
+        {
+            transform.Translate(speed * Time.deltaTime * Vector3.left);
+        }
+        
+
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float jumpForce = 10f;
-    public float gravityMultiplier = 1f;
+    public float jumpForce = 5f;
+    public float gravityMultiplier = 30f;
+    public bool isGameOver;
     private Rigidbody rb;
     private InputAction jumpAction;
-
+    private bool isGrounded = false;
     void Awake()
     {
+        isGameOver = false;
         rb = GetComponent<Rigidbody>();
         jumpAction = InputSystem.actions.FindAction("Jump");
     }
@@ -17,15 +19,32 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Physics.gravity *= gravityMultiplier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (jumpAction.triggered)
-        {
 
+        if (jumpAction.triggered && isGrounded == true && isGameOver == false)
+        {
+            rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+            isGrounded = false ;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            isGameOver = true;
+            Debug.Log("Game Ovah");
+            //Time.timeScale = 0f;
+        }
+        
     }
 }
